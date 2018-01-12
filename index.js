2c26aa2e6272483ad273af894af36c534c96ae65
craig stevens <c_stevens44@hotmail.com>
2018-01-12 05:08:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code below!
+
+function happyHolidays(String) {
+  return "Happy holidays!"
+}
+
+
+function happyHolidaysTo(name) {
+if ( name === "you")
+return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(string)(name) {
+  if bv
+  
